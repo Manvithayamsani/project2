17ec5c50868588d5aab1eb1633ba103dba2f4c44
Manvithayamsani <manvithayamsani@gmail.com>
2024-10-30 15:47:19 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 public class Sample
 {
-	public static void main(String args[]
-			{
-				System.out.print("Yamsani Manvitha");
-			}
+	public static void main(String args[])
+	{
+		System.out.print("Yamsani Manvitha");
+	}
 }
