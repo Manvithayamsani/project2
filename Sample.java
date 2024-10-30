9058e3a197cdc358500f9c3e1864a9816b40d0bf
Manvithayamsani <manvithayamsani@gmail.com>
2024-10-30 15:06:43 +0530

    sample

---
+++ Sample.java
@@ -0,0 +1,30 @@
+
+
+
+
+i
+public class Sample
+{
+	public static void main(String args[]
+			{
+				System.out.print("Yamsani Manvitha");
+			}
+}
+KITS@DESKTOP-8D7ERIG MINGW64 ~
+$
+
+
+
+
+KITS@DESKTOP-8D7ERIG MINGW64 ~
+$
+
+
+
+
+KITS@DESKTOP-8D7ERIG MINGW64 ~
+$
+
+
+
+
